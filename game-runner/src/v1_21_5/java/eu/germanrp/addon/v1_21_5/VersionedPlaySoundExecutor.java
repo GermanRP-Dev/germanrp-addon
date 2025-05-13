9dbc855465579c28f32d851f3c2f1133801b0a7f
Azusagawachan <73001005+AhegaHOE@users.noreply.github.com>
2025-05-13 08:35:14 +0200

    Rename and refactor ExampleChatExecutor to PlaySoundExecutor
    Replaced `sendHelloWorld` functionality with `playNotePlingSound` to play a sound event. Adjusted all relevant classes and dependencies to reflect the new PlaySoundExecutor implementation, including integration with PlantListener.

---
+++ game-runner/src/v1_21_5/java/eu/germanrp/addon/v1_21_5/VersionedPlaySoundExecutor.java
@@ -0,0 +1,26 @@
+package eu.germanrp.addon.v1_21_5;
+
+import eu.germanrp.addon.core.executor.PlaySoundExecutor;
+import net.labymod.api.models.Implements;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.sounds.SoundEvents;
+
+import javax.inject.Singleton;
+
+@Singleton
+@Implements(PlaySoundExecutor.class)
+public class VersionedPlaySoundExecutor implements PlaySoundExecutor {
+
+    @Override
+    public void playNotePlingSound() {
+        final LocalPlayer player = Minecraft.getInstance().player;
+
+        if (player == null) {
+            return;
+        }
+
+        player.playSound(SoundEvents.NOTE_BLOCK_PLING.value());
+    }
+
+}
