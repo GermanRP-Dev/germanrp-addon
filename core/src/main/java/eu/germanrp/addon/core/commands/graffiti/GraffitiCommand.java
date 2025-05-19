f3707d1e1aa32e060482bb37dd7344bd5a7c8365
Azusagawachan <73001005+AhegaHOE@users.noreply.github.com>
2025-05-19 15:48:17 +0200

    Fixed command output

--- core/src/main/java/eu/germanrp/addon/core/commands/graffiti/GraffitiCommand.java
+++ core/src/main/java/eu/germanrp/addon/core/commands/graffiti/GraffitiCommand.java
@@ -1,13 +1,14 @@
 package eu.germanrp.addon.core.commands.graffiti;
 
 import eu.germanrp.addon.core.GermanRPAddon;
+import eu.germanrp.addon.core.widget.GraffitiHudWidget.GraffitiHudWidgetConfig;
 import net.labymod.api.client.chat.command.Command;
 
 public class GraffitiCommand extends Command {
 
-    public GraffitiCommand(final GermanRPAddon addon) {
+    public GraffitiCommand(final GermanRPAddon addon, final GraffitiHudWidgetConfig config) {
         super("graffiti");
-        this.withSubCommand(new GraffitiListCommand(addon));
+        this.withSubCommand(new GraffitiListCommand(addon, config));
     }
 
     @Override
