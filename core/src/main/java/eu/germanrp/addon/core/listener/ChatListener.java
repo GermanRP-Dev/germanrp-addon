11925de93b20550de78f3a4d89cbc1aa778bdc56
Eckiges <eckigesjump@gmail.com>
2025-05-21 00:21:28 +0200

    changed to MajorWidgetUpdateEvent

--- core/src/main/java/eu/germanrp/addon/core/listener/ChatListener.java
+++ core/src/main/java/eu/germanrp/addon/core/listener/ChatListener.java
@@ -1,7 +1,10 @@
 package eu.germanrp.addon.core.listener;
 
 import eu.germanrp.addon.core.GermanRPAddon;
+import eu.germanrp.addon.core.common.events.MajorWidgetUpdateEvent;
 import eu.germanrp.addon.core.widget.MajorEventWidget;
+import net.labymod.api.Laby;
+import net.labymod.api.client.gui.hud.hudwidget.text.TextLine;
 import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.client.chat.ChatReceiveEvent;
 
@@ -23,6 +26,9 @@ public class ChatListener {
     }
     @Subscribe
     public void onChatReceiveMajorEvent(ChatReceiveEvent e){
+        if(this.majorEventWidget.isMajorEvent()){
+            return;
+        }
         String m = e.chatMessage().getPlainText();
         final Matcher ApothekenRaubMatcher = APOTHEKEN_RAUB.getPattern().matcher(m);
         final Matcher ShopRaubMatcher = SHOP_RAUB.getPattern().matcher(m);
@@ -30,28 +36,25 @@ public class ChatListener {
         final Matcher JuwelenRaubMatcher = JUWELEN_RAUB.getPattern().matcher(m);
 
         if(ShopRaubMatcher.find()){
-            majorEventWidget.setMajorEvent(true);
-            majorEventWidget.getEventNameLine().updateAndFlush("Shopraub");
-            majorEventWidget.setCountdownTarget(ZonedDateTime.now().plusMinutes(3));
+            Laby.fireEvent(new MajorWidgetUpdateEvent("Shopraub", 3));
+            return;
         }
+
         if (ApothekenRaubMatcher.find()){
-            majorEventWidget.setMajorEvent(true);
-            majorEventWidget.getEventNameLine().updateAndFlush("Apothekenraub");
-            majorEventWidget.setCountdownTarget(ZonedDateTime.now().plusMinutes(8));
+            Laby.fireEvent(new MajorWidgetUpdateEvent("Apothekenraub", 8));
+            return;
         }
-        if (JuwelenRaubMatcher.find()){
-            majorEventWidget.setMajorEvent(true);
-            majorEventWidget.getEventNameLine().updateAndFlush("Juwelenraub");
-            majorEventWidget.setCountdownTarget(ZonedDateTime.now().plusMinutes(3));
+
+        if (JuwelenRaubMatcher.find()) {
+            Laby.fireEvent(new MajorWidgetUpdateEvent("Juwelenraub", 3));
+            return;
         }
+
         if (BombeStartMatcher.find()){
-            majorEventWidget.setMajorEvent(true);
-            majorEventWidget.getEventNameLine().updateAndFlush("Bombe");
-            majorEventWidget.setCountdownTarget(ZonedDateTime.now().plusMinutes(10));
+            Laby.fireEvent(new MajorWidgetUpdateEvent("Bombe", 10));
+            return;
         }
-
-        
-
     }
+
 
 }
