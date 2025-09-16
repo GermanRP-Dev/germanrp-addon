542fecd02fd27253655b628fed97be213f4fe783
EtwasEckiges <105496469+EtwasEckiges@users.noreply.github.com>
2025-09-16 23:09:09 +0200

    refactored namespace to "germanrpeuaddon"

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ labyMod {
     }
 
     addonInfo {
-        namespace = "germanrpaddon"
+        namespace = "germanrpeuaddon"
         displayName = "GermanRP Addon"
         author = "GermanRP Developers"
         description = "Verbessert und erleichtert das Spielerlebnis auf GermanRP.eu"
