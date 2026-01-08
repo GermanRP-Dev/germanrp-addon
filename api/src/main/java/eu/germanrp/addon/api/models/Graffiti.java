ad1e8099ac901116bd30ff67ddb4f5dbaa56985a
Azusagawachan <73001005+AhegaHOE@users.noreply.github.com>
2026-01-08 11:09:27 +0100

    fix: removed unavailable graffities

--- api/src/main/java/eu/germanrp/addon/api/models/Graffiti.java
+++ api/src/main/java/eu/germanrp/addon/api/models/Graffiti.java
@@ -16,15 +16,13 @@ public enum Graffiti implements Nearest {
     REICHENBRUECKE("Autobahnbrücke (Reichenviertel)", new DefaultPosition(458, 71, 263)),
     JVA("JVA (Altstadt)", new DefaultPosition(-798, 70, -754)),
     POST("Postzentrale (Downtown)", new DefaultPosition(496, 68, -1096)),
-    SCHULE("Schule (Downtown)", new DefaultPosition(768, 68, -1061)),
     TIEFGARAGE_ALTENHEIM("Tiefgarage (Polizeipräsidium)", new DefaultPosition(498, 58, -781)),
     TIEFGARAGE_PLATTENBAU("Tiefgarage (Plattenbau)", new DefaultPosition(699, 57, -1111)),
     TIEFGARAGE_HOLDEN("Tiefgarage (Holden-Tower)", new DefaultPosition(509, 61, -850)),
     APO1("Apotheke-1 (Downtown)", new DefaultPosition(646, 68, -943)),
     U_BAHN_ALTSTADT("U-Bahn (Altstadt)", new DefaultPosition(-474, 52, -888)),
     TRAM("Tram-Unterführung (Altstadt)", new DefaultPosition(-635, 70, -944)),
-    U_BAHN_ASIA("U-Bahn (Asia-Viertel)", new DefaultPosition(-464, 70, 935)),
-    U_BAHN_STRANDPASSAGE("U-Bahn (Strandpassage)", new DefaultPosition(552, 52, 795));
+    U_BAHN_ASIA("U-Bahn (Asia-Viertel)", new DefaultPosition(-464, 70, 935));
 
     private final String name;
     private final Position position;
