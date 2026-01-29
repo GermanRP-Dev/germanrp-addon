a76cc20d549b2ff378dded0559c486cf5b9439b5
Azusagawachan <73001005+AhegaHOE@users.noreply.github.com>
2026-01-29 06:34:36 +0100

    feat: added reading id from chat

--- api/src/main/java/eu/germanrp/addon/api/models/CharacterInfo.java
+++ api/src/main/java/eu/germanrp/addon/api/models/CharacterInfo.java
@@ -2,6 +2,13 @@ package eu.germanrp.addon.api.models;
 
 import java.util.UUID;
 
+/**
+ * Character info
+ *
+ * @param uniqueId   the unique id of the player that this information belongs to
+ * @param playerName the Minecraft Name of the player
+ * @param name       the player's character name
+ */
 public record CharacterInfo(
         UUID uniqueId,
         String playerName,
