60253b052d8cac08f93bf65f319e46af6ecd21cd
Azusagawachan <73001005+AhegaHOE@users.noreply.github.com>
2026-01-29 08:31:33 +0100

    fix: removed bogus package

--- net/labymod/api/client/gui/screen/widget/widgets/renderer/HrWidget.java
+++
@@ -1,9 +0,0 @@
-package net.labymod.api.client.gui.screen.widget.widgets.renderer;
-
-import net.labymod.api.client.gui.lss.property.annotation.AutoWidget;
-import net.labymod.api.client.gui.screen.widget.SimpleWidget;
-
-@AutoWidget
-public class HrWidget extends SimpleWidget {
-
-}
